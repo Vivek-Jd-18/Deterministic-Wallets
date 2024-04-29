1908783f043aba2d9cb26f5ab1713b3762753ef7
Vivek-Jd-18 <vivekdhage.vhits@gmail.com>
2024-04-29 18:15:32 +0530

    main stuff

---
+++ rpc.ts
@@ -0,0 +1,7 @@
+const sepoliaRPC = "https://eth-sepolia.g.alchemy.com/v2/R6OILRKTXDUEBz3gObRmArSbNIieviHL"
+const baseRPC = "https://base-sepolia.g.alchemy.com/v2/n-ev3J8wA27ltbz641pguBCnvyZoBeNw"
+
+export  {
+    sepoliaRPC,
+    baseRPC
+}
